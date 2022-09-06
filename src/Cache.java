12f0793fd5d1b1e34bdd1a58ecef03948d2e7a86
Boris Blagojevic <boris.blagojevicc@hotmail.com>
2022-09-06 13:37:14 +0200

    Visualizing Data Fom User Generated Objects
    - Passing through all user generated data and displaying it in a cli.
    - Cash class (TO DO)

--- src/Cache.java
+++ src/Cache.java
@@ -1,9 +1,5 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
-
-import java.io.Serializable;
-import java.util.*;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 public class Cache {
 
@@ -17,99 +13,46 @@ public class Cache {
 
     public static void main(String[] args) {
         var map = new LinkedHashMap<String, Object>();
-        ObjectTemplate a = new ObjectTemplate();
 
-        a.fields.put("New", "Map");
+        // creating and fixing objects
+        ObjectTemplate a = new ObjectTemplate();
+        a.put("New", "Map");
 //        a.fields.put("Array", new int[]{1, 2 ,4 , 5});
-        a.fields.put("Boolean", true);
-        a.fields.put("Bool", false);
+        a.put("Boolean", true);
+        a.put("Bool", false);
 
         ObjectTemplate b = new ObjectTemplate();
-        b.fields.put("Integer", 123);
-        b.fields.put("Double", 123.34);
+        b.put("Integer", 123);
+        b.put("Double", 123.34);
 
-        a.fields.put("Numbers", b);
+        a.put("Numbers", b);
 
-        a.fields.put("NotNumber", null);
+        a.put("NotNumber", null);
 
         ObjectTemplate as = new ObjectTemplate();
         ObjectTemplate car = new ObjectTemplate();
 
-        car.fields.put("Engine", "inline");
-        car.fields.put("Doors", 2);
-        car.fields.put("Wheels", 4);
-        car.fields.put("Color", "Red");
+        car.put("Engine", "inline");
+        car.put("Doors", 2);
+        car.put("Wheels", 4);
+        car.put("Color", "Red");
 
         as.put("Car parts", car);
 
-        a.fields.put("Car", as);
+
+        a.put("Car", as);
 
         map.put("Planets", a);
 
+        // to do
         for (Map.Entry<String, Object> i : map.entrySet())
             System.out.println("\"" + i.getKey() + "\" : \n" + i.getValue());
+        System.out.println("}");
 
+        ObjectTemplate ob = (ObjectTemplate) map.get("Planets");
 
+        System.out.println( "Numbers : {\n" +
+        ob.get("Numbers") + "}");
     }
 }
 
-class ObjectTemplate {
-    static int lvl = 1;
-    static boolean first = false;
-    LinkedHashMap<String, Object> fields = new LinkedHashMap<>();
-
-    ObjectTemplate() {
-    }
-
-    ;
-
-    public void put(String key, Object ob) {
-        fields.put(key, ob);
-    }
-
-    public Object get(String key) {
-        return fields.get(key);
-    }
-
-    @Override
-    public String toString() {
-        if (fields.size() == 0) return "{}";
-
-        StringBuilder string = new StringBuilder("");
-
-        if (!first)
-            for (int i = lvl; i > 0; i--)
-                string.append("\t");
-
-
-//        Iterator<Map.Entry<String, Object>> entry = fields.entrySet().iterator();
-
-
-        for (Map.Entry<String, Object> entry : fields.entrySet()) {
-            string.append("\"").append(entry.getKey()).append("\" : ");
-
-
-            if (entry.getValue() == null) {
-                string.append(" {},\n");
-
-            } else if (entry.getValue() instanceof ObjectTemplate) {
-                ObjectTemplate temp = (ObjectTemplate) entry.getValue();
-                first = false;
-                lvl++;
-                string.append("{\n").append(temp.toString()).append("\n}\n");
-
-
-            } else{
-                string.append("\"").append(entry.getValue()).append("\",\n");
-
-            for (int i = lvl; i > 0; i--)
-                string.append("\t");
-
-            first = true;
-            }
-        }
-
-
-        return string.toString();
-    }
-}
---
+++ src/ObjectTemplate.java
@@ -0,0 +1,51 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class ObjectTemplate {
+    static int objectHierarchy = 1;
+    private LinkedHashMap<String, Object> fields = new LinkedHashMap<>();
+
+    public void put(String key, Object ob) {
+        fields.put(key, ob);
+    }
+    public Object get(String key) {
+        return fields.get(key);
+    }
+
+    @Override
+    public String toString() {
+        if (fields.size() == 0) return "{}";
+
+        StringBuilder string = new StringBuilder("");
+        int tabsCount = objectHierarchy++;
+
+        for (Map.Entry<String, Object> entry : fields.entrySet()) {
+
+            string.append(appendTabs(tabsCount)); // first
+            string.append("\"").append(entry.getKey()).append("\" : ");
+
+            if (entry.getValue() == null) {
+                string.append(" {},\n");
+
+            } else if (entry.getValue() instanceof ObjectTemplate) {
+                ObjectTemplate temp = (ObjectTemplate) entry.getValue();
+
+                string.append("{\n").append(temp);
+                string.append(appendTabs(tabsCount));
+                string.append("},\n");
+                objectHierarchy--;
+
+            } else string.append("\"").append(entry.getValue()).append("\",\n");
+        }
+        return string.toString();
+    }
+
+    private String appendTabs(int tabsCount) {
+        StringBuilder string = new StringBuilder();
+
+        for (int i = tabsCount; i > 0; i--)
+            string.append("\t");
+
+        return string.toString();
+    }
+}
