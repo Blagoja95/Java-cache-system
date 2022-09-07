1d218bef8f61f570b77f3e83ac830f6b81e47369
Boris Blagojevic <boris.blagojevicc@hotmail.com>
2022-09-07 12:27:23 +0200

    create objects in sequence, random access create anf fields to do

--- src/Cache.java
+++ src/Cache.java
@@ -4,10 +4,12 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Cache {
+    static LinkedHashMap floorMap = new LinkedHashMap<String, Object>();
+    static LinkedHashMap objectDataPointer;
 
     static void read(Map<String, Object> ob) {
         for (Map.Entry i : ob.entrySet())
-            System.out.println("\"" + i.getKey() + "\" : \n" + i.getValue());
+            System.out.println("\"" + i.getKey() + "\" : {\n" + i.getValue());
         System.out.println("}");
     }
 
@@ -17,28 +19,48 @@ public class Cache {
                 var createPath = new LinkedList<>
                         (List.of(commands[3].replace("\"", "").split("\\W")));
 
+                // create first
+                String tempPath = createPath.getFirst();
+                if (!floorMap.containsKey(tempPath)){
+                    ObjectTemplate tempObject = new ObjectTemplate();
+                    floorMap.put(tempPath, tempObject);
+                    createPath.removeFirst();
+                    objectDataPointer = tempObject.fields;
+                }q
 
+                if (createPath.size() == 0) return;
+                createObject(createPath);
                 break;
 
-            case "filed":
-                System.out.println("Creating filed");
+            case "field":
+                System.out.println("Creating field");
                 break;
 
             default:
-
+                System.out.println("Missing commands");
+                break;
         }
     }
 
-    static ObjectTemplate createObject() {
-        return new ObjectTemplate();
+    static void createObject(LinkedList list) {
+
+//        if (objectDataPointer.containsKey(list.getFirst()))
+
+        ObjectTemplate tempOb = new ObjectTemplate();
+        objectDataPointer.put(list.getFirst(), tempOb);
+        objectDataPointer = tempOb.fields;
+        list.removeFirst();
+
+        if (list.size() != 0) createObject(list);
+
+        objectDataPointer = floorMap; // reset pointer;
     }
 
 
     public static void main(String[] args) {
-        var floorMap = new LinkedHashMap<String, Object>();
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            while ( true ) {
+            while (true) {
                 String input = br.readLine();
 
                 if (input.equals("q") || input.equals("quit")
@@ -47,12 +69,13 @@ public class Cache {
                 if (input.length() == 0)
                     System.out.println("Command can't be empty! Try: cache create object \"planet.name\"");
 
-                String[] commands = input.split("\\s");
+                String[] commands = input.split("\\s+");
 
-                if (commands.length >= 4  && commands[0].equals("cache"))
+                if (commands.length >= 4 && commands[0].equals("cache"))
                     switch (commands[1]) {
                         case "create":
                             create(commands);
+                            read(floorMap);
                             break;
 
                         case "read":
